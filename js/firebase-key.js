6d24bdcacd49046c56d18ecd263d91f63b3cb0be
gunalkanna <gunalkanna@gmail.com>
2017-07-26 14:31:10 +0800

    Add files via upload

--- js/firebase-key.js
+++ js/firebase-key.js
@@ -1,5 +1,5 @@
 var cs = { 
-  "apiKey": '', 
-  "authDomain": '', 
-  "databaseURL": '' 
+  "apiKey": 'AIzaSyCE8GQc0bRUTQ_GU_K86NqbE6jv5ogLaN4', 
+  "authDomain": 'ttchamp-6b3a7.firebaseapp.com', 
+  "databaseURL": 'https://ttchamp-6b3a7.firebaseio.com' 
 };
